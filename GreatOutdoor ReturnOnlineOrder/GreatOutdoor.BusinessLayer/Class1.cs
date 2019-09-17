0e4942bb4300b72db4cf93474ab6a058b5a90970
Ayush0087 <54570255+Ayush0087@users.noreply.github.com>
2019-09-17 09:36:10 +0530

    nb

---
+++ GreatOutdoor ReturnOnlineOrder/GreatOutdoor.BusinessLayer/Class1.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GreatOutdoor.DataAccessLayer;
+using GreatOutdoor.Entity;
+using GreatOutdoor.Exception;
+
+namespace GreatOutdoor.BusinessLayer
+{
+   
+    public class OnlineReturnBL
+    {
+        private static bool ValidateGuest(OnlineReturnBL onlineReturn)
+        {
+            StringBuilder sb = new StringBuilder();
+            bool valiOnlineReturnBL = true;
+            if (OnlineReturnBL.ReturnID <= 0)
+            {
+                validOnlineReturnBL = false;
+                sb.Append(Environment.NewLine + "Invalid ReturnOnlineOrder ID");
+
+            }
+            if (OnlineReturnBL.OnlineReturnName == string.Empty)
+            {
+                validOnlineReturnBL = false;
+                sb.Append(Environment.NewLine + "ReturnOnlineOrder Name Required");
+
+            }
+            if (OnlineReturnBL.OnlineReturnContactNumber.Length < 10)
+            {
+                validOnlineReturn = false;
+                sb.Append(Environment.NewLine + "Required 10 Digit Contact Number");
+            }
+            if (validOnlineReturn == false)
+                throw new GreatOutdoorException(sb.ToString());
+            return validOnlineReturn;
+        }
+
+        public static bool AddOnlineReturnBL(OnlineReturnBL newOnlineReturn)
+        {
+            bool onlineReturnAdded = false;
+            try
+            {
+                if (ValidateOnlineReturn(newOnlineReturn))
+                {
+                    OnlineReturnDAL onlineReturnDAL = new OnlineReturnDAL();
+                    onlineReturnAdded = onlineReturnDAL.AddOnlineReturnDAL(newOnlineReturn);
+                }
+            }
+            catch (GreatOutdoorException)
+            {
+                throw;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+
+            return onlineReturnAdded;
+        }
+
+        public static List<OnlineReturnBL> GetAllGuestsBL()
+        {
+            List<OnlineReturnBL> onlineReturnList = null;
+            try
+            {
+                OnlineReturnDAL onlineReturn = new OnlineReturnDAL();
+                onlineReturnList = OnlineReturnDAL.GetAllReturnOnlineOrderDAL();
+            }
+            catch (GreatOutdoorException ex)
+            {
+                throw ex;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            return onlineReturnList;
+        }
+
+        public static OnlineReturnBL SearchOnlineReturnBL(int searchReturnID)
+        {
+            OnlineReturnBL searchOnlineReturn = null;
+            try
+            {
+                OnlineReturnDAL onlineReturn = new OnlineReturnDAL();
+                searchOnlineReturn = OnlineReturnDAL.SearchOnlineReturnDAL(searchReturnID);
+            }
+            catch (GreatOutdoorException ex)
+            {
+                throw ex;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            return searchOnlineReturn;
+
+        }
+
+        public static bool UpdateOnlineReturnBL(OnlineReturnBL updateonlineReturn)
+        {
+            bool onlineReturnUpdated = false;
+            try
+            {
+                if (ValidateOnlineReturn(updateOnlineReturn))
+                {
+                    OnlineReturnDAL onlineReturn = new OnlineReturnDAL();
+                    onlineReturnUpdated = OnlineReturnDAL.UpdateOnlineReturnDAL(updateonlineReturn);
+                }
+            }
+            catch (GreatOutdoorException)
+            {
+                throw;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+
+            return onlineReturnUpdated;
+        }
+
+        public static bool DeleteOnlineReturnBL(int deleteReturnID)
+        {
+            bool onlineReturnDeleted = false;
+            try
+            {
+                if (deleteReturnID > 0)
+                {
+                    OnlineReturnDAL onlineReturn = new OnlineReturnDAL();
+                    onlineReturnDeleted = OnlineReturnDAL.DeleteReturnOnlineOrderDAL(deleteReturnID);
+                }
+                else
+                {
+                    throw new GreatOutdoorException("Invalid ReturnOnlineOrder ID");
+                }
+            }
+            catch (GreatOutdoorException)
+            {
+                throw;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+
+            return onlineReturnDeleted;
+        }
+       
+    }
+}
---
+++ GreatOutdoor ReturnOnlineOrder/GreatOutdoor.DataAccessLayer/Class1.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GreatOutdoor.BusinessLayer;
+using GreatOutdoor.Entity;
+using GreatOutdoor.Exception;
+
+namespace GreatOutdoor.DataAccessLayer
+{
+    public class OnlineReturnDAL
+    {
+        public static List<OnlineReturnDAL> onlineReturnList = new List<OnlineReturnDAL>();
+
+        public bool AddOnlineReturnDAL(OnlineReturnDAL newonlineReturn)
+            //adding return order
+        {
+            bool onlineReturnAdded = false;
+            try
+            {
+                onlineReturnList.Add(newonlineReturn);
+                onlineReturnAdded = true;
+            }
+            catch (SystemException ex)
+            {
+                throw new Exception(ex.Message);
+            }
+            return onlineReturnAdded;
+
+        }
+
+        public List<OnlineReturnDAL> GetAllGuestsDAL()
+        {
+            return onlineReturnList;
+        }
+
+        public OnlineReturnDAL SearchOnlineReturnDAL(int searchReturnID)
+            //returnonlineorderby returnID
+        {
+            OnlineReturnDAL searchOnlineReturnDAL = null;
+            try
+            {
+                foreach (OnlineReturnDAL item in onlineReturnList)
+                {
+                    if (item.ReturnID == searchReturnID)
+                    {
+                        searchOnlineReturnDAL = item;
+                    }
+                }
+            }
+            catch (SystemException ex)
+            {
+                throw new Exception(ex.Message);
+            }
+            return searchOnlineReturnDAL;
+        }
+
+        public List<OnlineReturnDAL> GetOnlineReturnByNameDAL(string onlineReturnName)
+        {
+            List<OnlineReturnDAL> searchOnlineReturn = new List<OnlineReturnDAL>();
+            try
+            {
+                foreach (OnlineReturnDAL item in onlineReturnList)
+                {
+                    if (item.ReturnID == onlineReturnName)
+                    {
+                       searchOnlineReturn.Add(item);
+                    }
+                }
+            }
+            catch (SystemException ex)
+            {
+                throw new Exception(ex.Message);
+            }
+            return searchOnlineReturn;
+        }
+
+        public bool UpdateReturnOnlineOrderDAL(OnlineReturnDAL updateonlineReturn)
+        {
+            bool onlineReturnUpdated = false;
+            try
+            {
+                for (int i = 0; i < onlineReturnList.Count; i++)
+                {
+                    if (onlineReturnList[i].ReturnID == updateonlineReturn.ReturnID)
+                    {
+                        updateonlineReturn.ReturnPurposeOfReturn = onlineReturnList[i].PurposeOfReturn;
+                        updateonlineReturn.ReturnMobileNumber =onlineReturnList[i].ReturnMobileNumber;
+                        onlineReturnUpdated = true;
+                    }
+                }
+            }
+            catch (SystemException ex)
+            {
+                throw new Exception(ex.Message);
+            }
+            return onlineReturnUpdated;
+
+        }
+
+        public bool DeleteOnlineReturnDAL(int deleteReturnID)
+        {
+            bool onlineReturnDeleted = false;
+            try
+            {
+                OnlineReturnDAL deleteOnlineReturn = null;
+                foreach (OnlineReturnDAL item in onlineReturnList)
+                {
+                    if (item.ReturnID == deleteReturnID)
+                    {
+                        deleteOnlineReturn = item;
+                    }
+                }
+
+                if (deleteOnlineReturn != null)
+                {
+                    onlineReturnList.Remove(deleteOnlineReturn);
+                    onlineReturnDeleted = true;
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+            return onlineReturnDeleted;
+
+        }
+
+    }
+}
